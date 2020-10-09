19b68309890de0cc5924f994c597aad6db4214ab
camilaborinsky <cborinsky@itba.edu.ar>
2020-10-09 20:25:54 -0300

    botones google facebook twitter, login, mejora en spacing y padding

--- routina/src/components/LoginPopOver.vue
+++ routina/src/components/LoginPopOver.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="text-center">
+  <div class="text-center ma-2">
     <v-menu v-model="menu"
         :rounded="0"  
         :close-on-content-click="false"
@@ -14,79 +14,111 @@
       </template>
 
       <v-card color="grey darken-3" >
-        <v-layout-row-wrap>
+        <v-row  class="ma-2">
           <v-spacer></v-spacer>
-          <v-icon small @click.stop="menu=!menu">mdi-close</v-icon>
-        </v-layout-row-wrap>
-        <v-card-text> 
+          <v-col cols="auto">
+            <v-icon small left @click.stop="menu=!menu">mdi-close</v-icon>
+          </v-col>
+        </v-row>
+        <v-card-text class="px-2 ma-0"> 
          <v-text-field  
-                placeholder="account"
+                placeholder="Email address"
                 solo
                 rounded
                 dense
                 light
+                clearable
           ></v-text-field>
           <v-text-field
                 :type="showPassword? 'text' : 'password'"   
-                placeholder="password"
+                placeholder="Password"
                 solo
                 rounded
                 dense
                 light
+                clearable
                 :append-icon="showPassword? 'mdi-eye':'mdi-eye-off'"
                 @click:append="showPassword=!showPassword"
           ></v-text-field> 
         </v-card-text>
-        <v-container >
-          <v-row align-start no-gutters>
-            <v-layout-col-wrap align-start>
-              <!-- <v-card> -->
-                <v-checkbox
-                  dark
-                  label='Stay logged in'>
-                </v-checkbox>
-              <!-- </v-card> -->
-            </v-layout-col-wrap>
-            <v-layout-col-wrap align-start>
-                Forgot Password?
-            </v-layout-col-wrap>
+        <v-container>
+          <v-row align-start 
+                  no-gutters
+                  class="pa-0 ma-0">
+            <v-row
+            :align="start"
+            no-gutters>
+                <v-col align-baseline>
+                  <v-checkbox
+                    v-model="chbLogin"
+                    color="orange darken-3"
+                    dark
+                    label='Stay logged in'>
+                  </v-checkbox> 
+                </v-col>
+                <v-col  align-baseline>
+                  <p class="blue--text">Forgot Password?</p>
+                </v-col>
+                
+            </v-row>
           </v-row>
-          
         </v-container>   
-        
-       
-        <v-card-text>
-        
-
-        
-        
-        <v-btn v-if='options.isLoggingIn' @click.prevent='' block type='submit'> Sign in</v-btn>
-        <div v-if='options.isLoggingIn'>
-              Don't have an account?
-              <v-btn 
-                light
-                @click='options.isLoggingIn = false'
-               > Sign up</v-btn>
-        </div> 
-        </v-card-text>
-        <v-card-actions>
+        <template>
+          <div class="text-center">
+            <v-btn
+              rounded
+              width="50%"
+              color="orange darken-3"
+              light
+              class="button font-weight-bold"
+            >Log in
+            </v-btn>
+          </div>
+        </template>
+        <v-row align-content="center">
+          <v-col align="center  ">
+            <p class="font-weight-medium">or continue with</p>  
+          </v-col>
+        </v-row>
+        <v-row align="center"
+               justify="center"
+                no-gutters
+                class="ma-1">
           <v-spacer></v-spacer>
-
-          <v-btn
-            text
-            @click="menu = false"
+          <v-col
+            v-for="service in loginProvs"
+            :key="service.name"
+            align="center"
+            class="ma-1"
           >
-            Cancel
-          </v-btn>
-          <v-btn
-            color="primary"
-            text
-            @click="menu = false"
-          >
-            Save
-          </v-btn>
-        </v-card-actions>
-
+          <v-responsive :aspect-ratio="1/1">
+              <v-btn
+              align="center"
+              justify="center"
+              color="white"
+              filled
+              width="60"
+              height="60"
+              >
+               <v-img :src="require(`../assets/${service.icon}`)" contain height="40px" width="40px"/>   
+              </v-btn>
+            </v-responsive>
+          </v-col>
+          <v-spacer></v-spacer>
+          
+        </v-row>  
+        <v-card-text>
+          <v-btn v-if='options.isLoggingIn' 
+                @click.prevent='' 
+                block type='submit'> Sign in</v-btn>
+          <div v-if='options.isLoggingIn'>
+                Don't have an account?
+                <v-btn 
+                  light
+                  @click='options.isLoggingIn = false'
+                > Sign up</v-btn>
+          </div> 
+        </v-card-text>
       </v-card>
     </v-menu>
   </div>
@@ -102,11 +134,15 @@ export default {
       message: false,
       hints: true,
       options:{
-      shouldStayLoggedIn:true,
-      isLoggingIn:true,
-      
-    },
-    showPassword:false,
+        shouldStayLoggedIn:true,
+        isLoggingIn:true,
+      },
+      showPassword:false,
+      loginProvs: [
+        { name: "Google",icon:"google.png", route: "/" },
+        { name: "Facebook", icon:"facebook.png", route: "/explore" },
+        { name: "Twitter", icon:"twitter.png", route: "/routines" },
+      ],
     }),
     offset:true,
     options:{
