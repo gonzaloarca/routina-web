55d11bf93aebb10a9d18c331ea107baa61bd11ea
camilaborinsky <cborinsky@itba.edu.ar>
2020-10-21 01:15:47 -0300

    explore tabs

---
+++ routina/src/components/ExploreTabHead.vue
@@ -0,0 +1,71 @@
+<template>
+    <div>
+       
+        <v-card class="tab-head" tile>
+            <v-card class="tab-icon" tile background-color="focus? orange darken-3 : $dark-grey">
+                <v-icon color="white">{{icon}}</v-icon>
+            </v-card>
+            <span height="100%" class="cont-text" >
+                <h3>{{name}}</h3>
+            </span>
+            
+        </v-card>
+    </div>
+  
+</template>
+<script>
+    export default{
+        name:"ExploreTabhead",
+        props:{
+            name:{
+                type:String,
+                required:true,
+            },
+            icon:{
+                type:String,
+                required:true,
+            },
+            focus:{
+                type:Boolean,
+                required:true,
+            },
+        }
+    };
+</script>
+
+
+<style scoped lang="scss">
+@import "~vuetify/src/styles/styles.sass";
+    $light-grey:#575757;
+    $dark-grey:#1d1d1d;
+    .tab-head{
+        position:relative;
+        background-color:$light-grey;
+        display:flex;
+        align-items: center;
+        justify-content: center;
+        width:150px;
+        height:40px;
+        
+    }
+    .tab-icon{
+        position:absolute;
+        left:0;
+        display:flex;
+        align-items: center;
+        justify-content:center;
+        // background-color:$dark-grey;
+        width:25%;
+        height:100%;
+    }
+
+    .cont-text{
+        position:absolute; 
+        width:75%; 
+        right:0; 
+        display:flex; 
+        align-items:center; 
+        justify-content:center;
+    }
+
+</style>
--- routina/src/views/Explore.vue
+++ routina/src/views/Explore.vue
@@ -1,18 +1,73 @@
 <template>
   <div class="Explore">
     <ExploreBanner />
+    <div class="explore-content">
+      <v-card class="content-container">
+        <h1 class="title-container">Explore routines by:</h1>
+        <v-tabs v-model="explore_tabs" background-color="black" fixed-tabs class="pb-0">
+          <v-tab
+            v-for="tab in tabs"
+            :key="tab.name"
+            @change="tab.focus=!tab.focus"
+            
+          >
+            <ExploreTabHead :name="tab.name" :icon="tab.icon" :focus="tab.focus" ></ExploreTabHead>
+          </v-tab>
+      </v-tabs>
+      <h1>hola</h1>
+      </v-card>
+      
+    </div>
   </div>
 </template>
 
 <script>
 import ExploreBanner from "../components/ExploreBannner";
+import ExploreTabHead from "../components/ExploreTabHead";
+
 
 export default {
   name: "Explore",
-  components: { ExploreBanner },
+  components: { ExploreBanner, ExploreTabHead },
+  data(){
+    return{
+      tabs:[
+        {name:"featured", icon:"mdi-star", focus:"true"},
+        {name:"duration", icon:"mdi-timer", focus:"false"},
+        {name:"type", icon:"mdi-star", focus:"false"},
+        {name:"difficulty level", icon:"mdi-star", focus:"false"},
+        {name:"equipment", icon:"mdi-star", focus:"false"},
+        {name:"muscle group", icon:"mdi-star", focus:"false"},
+      ],
+      
+    }
+  }
 };
 </script>
 
 <style scoped lang="scss">
+  @import "../sass/variables";
+  .Explore {
+    background-color: black;
+  }
+  .explore-content {
+    max-width: $content-container-width;
+    margin: auto;
+    padding: 30px 30px 30px 30px;
+  }
+
+  .title-container{
+    padding: 30px 30px 30px 30px;
+  }
+
+  .tab{
+    width:20%;
+  }
+  .content-container {
+    max-width: $content-container-width - 150px;
+    align-content: center;
+    margin: 0 auto 30px;
+    // padding: 30px 30px 30px 30px;
+  }
 
 </style>
