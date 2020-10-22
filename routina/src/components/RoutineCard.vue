8909596f242fbb61eadf1bc0b013a6ad15330209
AgustinJerusalinsky <ajerusalinsky@itba.edu.ar>
2020-10-22 02:27:01 -0300

    Revert "routine clickeable everywhere"
    This reverts commit 7b46103a68c6eba255af5ba28ed516837c764ff6.

--- routina/src/components/RoutineCard.vue
+++ routina/src/components/RoutineCard.vue
@@ -1,8 +1,6 @@
 <template>
-  <div class="routine-container">
-    
-
-    <v-card class="routine-card" v-on:click="overlay = true" tile>
+  <div class="routine-container" >
+    <v-card class="routine-card" tile>
       <div class="routine-info">
         <p class="text-uppercase text-caption font-weight-medium mb-0">
           <span class="type-label text-caption grey--text text--lighten-1 my-0"
@@ -40,26 +38,19 @@
         </p>
       </div>
     </v-card>
-    
-    <Card
-      :routine-data="routineData"
-      v-on:close-overlay="overlay = false"
-      :overlay="overlay"
-    />
   </div>
 </template>
 
 <script>
 import DifficultyLevel from "./DifficultyLevel.vue";
-import Card from "./Card.vue";
 export default {
   name: "RoutineCard",
-  components: { DifficultyLevel, Card },
-  data() {
-    return {
-      overlay: false,
+  data(){
+    return{
+      overlay:false,
     };
   },
+  components: { DifficultyLevel },
   props: {
     routineData: {
       type: Object,
@@ -106,6 +97,7 @@ $routine-info-width: 75px;
   height: $card-height;
   width: $card-width;
 }
+
 
 .routine-card {
   position: absolute !important;
