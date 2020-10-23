3be7a32b1dd480e38040e1caae235c3003ec8c46
camilaborinsky <cborinsky@itba.edu.ar>
2020-10-23 05:45:49 -0300

    banner user bien encaminado

--- routina/src/components/ProfileBanner.vue
+++ routina/src/components/ProfileBanner.vue
@@ -1,10 +1,41 @@
 <template>
-  <div class="settings-banner" background-color="primary" >
+  <div class="profile-banner" background-color="primary" >
     <div class="slider-wrapper">
-      <div class="settings-header" >
-        <h1 class="black--text">User</h1>
-      </div>
-   
+     <div class="content-container">
+       <div class="row-fmt">
+         <div>
+           <v-card class="big-card" tile>
+             hola
+           </v-card >
+           <div class="row-fmt">
+             <v-card class="small-card" tile>
+             hola
+            </v-card >
+            <v-card class="small-card" tile>
+             hola
+            </v-card >
+            <v-card class="small-card" tile>
+             hola
+            </v-card >
+           </div>
+         </div>
+         <v-card tile class="medium-card">
+           <h3 class="medium-title">Latest workout</h3>
+         </v-card>
+         <v-card tile class="medium-card">
+           <h2 class="medium-title">Most used routine</h2>
+         </v-card>
+       </div>
+        
+        <div class="row-fmt">
+        <v-btn
+              rounded
+              color="black"
+              class="button font-weight-bold"
+            ><h3 class="primary--text">Edit profile</h3>
+        </v-btn>
+      </div> 
+     </div>
     </div>
       
   </div>
@@ -23,19 +54,104 @@ export default {
 
 
 $y-displacement: 0px;
+$height: $banner-height+200px;
 
-.settings-banner{
-    height: $banner-height;
-    
-    
+.profile-banner{
+    height: $height;
+    background-color: $primary;
+}
+
+.col-fmt{
+  padding:10px 10px 10px 10px;
+}
+
+.row-format{
+  display:flex;
+  flex-direction:row;
+  width:100%;
+}
+
+
+.medium-card{
+  width:300px;
+  height:300px;
+  margin-left:20px;
+  margin-right:20px;
+  margin-top:10px;
+  margin-bottom:30px;
+  text-align: center;
+  padding-left:70px;
+  padding-right:70px;
+  .medium-title{
+    text-transform:uppercase;
+    color: grey;
+    font-size: 16px;
+    padding:10px 10px 10px 10px;
+  }
+
+}
+
+.big-card{
+  width:500px;
+  height:200px;
+  margin-left:20px;
+  margin-right:20px;
+  margin-top:10px;
+  margin-bottom:30px;
+  text-align: center;
+  padding-left:30px;
+  padding-right:30px;
+  .big-title{
+    text-transform:uppercase;
+    color: grey;
+    font-size: 36px;
+    padding:10px 10px 10px 10px;
+  }
+
+}
+
+.small-card{
+  width:150px;
+  height:70px;
+  margin-left:12px;
+  margin-right:12px;
+  margin-top:10px;
+  margin-bottom:30px;
+  text-align: center;
+  padding-left:15px;
+  padding-right:15px;
+  .big-title{
+    text-transform:uppercase;
+    color: grey;
+    font-size: 36px;
+    padding:10px 10px 10px 10px;
+  }
+
+}
+
+.row-fmt{
+  width:100%;
+  display:flex;
+  align-items:center;
+  justify-content:center;
+}
+.content-container{
+  position:absolute;
+  top:$nav-bar-height;
+  height:$height - $nav-bar-height;
+  display:flex; 
+  align-items:center; 
+  justify-content:center;
+  flex-direction:column;
+  width:100%;
 }
 
 .slider-wrapper{
     position:relative;
     width:100%;
-    height:$banner-height;
+    height:$height;
 
-    .settings-header {
+    .profile-header {
     height: 70px;
     width: 350px;
     position: absolute;
