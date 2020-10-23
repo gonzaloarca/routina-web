943e4ba0b7e0fc474485252b3e450035da98b77c
atormakh <atormakh@itba.edu.ar>
2020-10-23 18:19:12 -0300

    fix edit routine bugs

--- routina/src/views/EditRoutine.vue
+++ routina/src/views/EditRoutine.vue
@@ -434,6 +434,7 @@ export default {
       });
     },
     removeRound(roundIndex) {
+      this.tab--;
       this.routineData.rounds.splice(roundIndex, 1);
     },
     openExercise(roundIndex) {
